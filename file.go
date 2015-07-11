9e5e38afca3a058aa0c3a15fd1dc18d733ab9c4d
Pellaeon Lin <nfsmwlin@gmail.com>
2015-07-11 18:18:18 +0800

    Update# [WIP] add FileCache struct, which 1. handles mem and disk caches 2. grab data from gdrive transparently when cache miss

--- file.go
+++ file.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -13,6 +14,8 @@ import (
 	"github.com/pellaeon/skicka/gdrive"
 	"golang.org/x/net/context"
 )
+
+const CACHE_THRESHOLD_SIZE = 4096
 
 var _ fs.Node = (*File)(nil)
 
@@ -49,6 +52,7 @@ func (n File) Open(ctx context.Context, req *fuse.OpenRequest, resp *fuse.OpenRe
 		sk_file:       n.sk_file,
 		url:           "",
 		urlLastUpdate: time.Unix(0, 0),
+		cache:         NewFileCache(n.sk_file.Id),
 	}, nil
 }
 
@@ -58,6 +62,7 @@ type FileHandle struct {
 	sk_file       *gdrive.File
 	url           string // file download URL
 	urlLastUpdate time.Time
+	cache         *FileCache
 }
 
 var _ fs.HandleReleaser = (*FileHandle)(nil)
@@ -125,25 +130,135 @@ var _ fs.HandleReader = (*FileHandle)(nil)
 
 func (fh *FileHandle) Read(ctx context.Context, req *fuse.ReadRequest, resp *fuse.ReadResponse) error {
 	go SingleUpdateMetadataCache()
-	buf := make([]byte, req.Size)
-	reader, err := fh.GetContent()
-	if err != nil {
-		logger.Errorf("FileHandle Read GetContent failed: %v", err)
-		return err
-	}
-	n, err := reader.Read(buf)
-	reader.Close()
-	if err != nil && n == 0 {
-		logger.Errorf("Filehandle Read failed: %v", err)
-		return err
+	if req.Offset == 0 {
+		doneFirstReq := make(chan bool)
+		fh.doneReadToCache = make(chan bool)
+		go fh.LaunchBackgroundGetContent(req.Size, doneFirstReq, fh.doneReadToCache)
+		<-doneFirstReq
+		logger.Debugf("========got first block")
+		resp.Data = fh.fileCache[:req.Size]
+		req.Respond(resp)
+		return nil
 	} else {
-		resp.Data = buf
-		logger.Debugf("FileHandle Read() %d bytes", n)
-		if int64(n) != fh.sk_file.FileSize {
-			logger.Warningf("FileHandle Read() %d bytes but file has %d bytes", n, fh.sk_file.FileSize)
-		}
+		<-fh.doneReadToCache
+		logger.Debugf("====read from %d to %d", req.Offset, len(fh.fileCache))
+		resp.Data = fh.fileCache[req.Offset:len(fh.fileCache)]
+		req.Respond(resp)
 		return nil
 	}
+}
+func (fh *FileHandle) LaunchBackgroundGetContent(firstReqSize int, doneFirstReq, done chan<- bool) error {
+	//var cacheSize int64
+	//if CACHE_THRESHOLD_SIZE <= fh.sk_file.FileSize {
+	//	cacheSize = CACHE_THRESHOLD_SIZE
+	//} else {
+	//	cacheSize = fh.sk_file.FileSize
+	//}
+	fh.fileCache = make([]byte, fh.sk_file.FileSize)
+	reader, err := fh.GetContent()
+	if err != nil {
+		logger.Errorf("FileHandle LaunchBackgroundGetContent GetContent failed: %v", err)
+		return err
+	}
+	n, err := io.ReadFull(reader, fh.fileCache[:firstReqSize])
+	if err != nil && n != firstReqSize {
+		logger.Errorf("Filehandle LaunchBackgroundGetContent read first block failed: %v", err)
+		return err
+	} else {
+		doneFirstReq <- true
+		close(doneFirstReq)
+	}
+	logger.Debugf("==========start reading %d", len(fh.fileCache[firstReqSize:]))
+	n, err = io.ReadFull(reader, fh.fileCache[firstReqSize:])
+	logger.Debugf("==========end reading %d", n)
+	if err != nil && n != int(fh.sk_file.FileSize)-firstReqSize { //TODO possible int overflow
+		logger.Errorf("Filehandle LaunchBackgroundGetContent read remaining failed: %v", err)
+		return err
+	} else if err != nil {
+		logger.Debugf("======%+v", err)
+	} else {
+		done <- true
+		close(done)
+	}
+	logger.Debugf("==============background getcontent terminating")
+	return nil
+}
+
+type FileCache struct {
+	fileId           string
+	inMemorySegments []int64
+	onDiskSegments   []int64
+	memSegments      map[int64][]byte
+}
+
+func NewFileCache(id string) *FileCache {
+	return &FileCache{
+		fileId:           id,
+		inMemorySegments: make([]int64, 100), //XXX
+		onDiskSegments:   make([]int64, 100), //TODO: check segment files on disk and populate information
+		memSegments:      make(map[int64][]byte),
+	}
+}
+
+func (fc *FileCache) Read(start int64, p []byte) (int, error) {
+	end, n, segmentno := int64(0)
+	// search memory segments
+	for i := 0; i <= len(fc.inMemorySegments)-1; i += 2 {
+		if fc.inMemorySegments[i] <= start {
+			if fc.inMemorySegments[i+1] > start+len(p) {
+				logger.Warningf("MemorySegment smaller than requested")
+				end = fc.inMemorySegments[i+1]
+				n = fc.inMemorySegments[i+1] - fc.inMemorySegments[i]
+			} else {
+				end = start + len(p)
+				n = len(p)
+			}
+			segmentno = fc.inMemorySegments[i]
+			break
+		}
+	}
+	if end != 0 {
+		p = memSegments[segmentno][start-segmentno : end-segmentno]
+		return n, nil
+	} else {
+		// TODO: search disk segments
+		logger.Debugf("Reading not in memory segent")
+		for i := 0; i <= len(fc.onDiskSegments)-1; i += 2 {
+			if fc.onDiskSegments[i] <= start {
+				if fc.onDiskSegments[i+1] > start+len(p) {
+					logger.Warningf("MemorySegment smaller than requested")
+					end = fc.onDiskSegments[i+1]
+					n = fc.onDiskSegments[i+1] - fc.onDiskSegments[i]
+				} else {
+					end = start + len(p)
+					n = len(p)
+				}
+				segmentno = fc.onDiskSegments[i]
+				break
+			}
+		}
+		// open on disk cache file
+		cacheFileName := CACHEDIR + fc.fileId + "_" + string(segmentno)
+		cacheFile, err := os.Open(cacheFileName)
+		if err != nil {
+			logger.Errorf("Opening cache file %s failed: %+v", cacheFileName, err)
+			//TODO: handle error
+		}
+		n, err = cacheFile.ReadAt(p, start-segmentno) //XXX: len(p) != [start-segmentno : end-segmentno]
+		if err != nil && n == 0 {
+			logger.Errorf("Reading cache file %s failed: %+v", cacheFileName, err)
+		}
+		return n, nil
+	}
+	// no data found in mem nor disk, start download
+	//TODO
+}
+
+func (fc *FileCache) Write(start int64, p []byte) (int, error) {
+	//TODO
+	append(fc.inMemorySegments, start)
+	append(fc.inMemorySegments, start+len(p))
+	memSegments[start] = p
 }
 
 var _ fs.NodeAccesser = (*File)(nil)
